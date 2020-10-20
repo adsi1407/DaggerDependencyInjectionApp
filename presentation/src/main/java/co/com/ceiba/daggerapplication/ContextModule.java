8306b74a1649c6cab6ff99a80980c5e60d0d564c
David Santacoloma <adsi1407@gmail.com>
2020-10-20 15:15:27 -0500

    Agregar módulo para manejo del Contexto de Android

---
+++ presentation/src/main/java/co/com/ceiba/daggerapplication/ContextModule.java
@@ -0,0 +1,21 @@
+package co.com.ceiba.daggerapplication;
+
+import android.content.Context;
+
+import dagger.Module;
+import dagger.Provides;
+
+@Module
+public class ContextModule {
+
+    Context context;
+
+    public ContextModule (Context context) {
+        this.context = context;
+    }
+
+    @Provides
+    public Context getContext() {
+        return context;
+    }
+}
