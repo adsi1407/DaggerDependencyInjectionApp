c326cf18dca57112c817c816e3044892d4eef8e3
David Santacoloma <adsi1407@gmail.com>
2020-10-20 15:16:42 -0500

    Inyectar recursos con Dagger en la Actividad principal

--- presentation/src/main/java/co/com/ceiba/daggerapplication/MainActivity.java
+++ presentation/src/main/java/co/com/ceiba/daggerapplication/MainActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import javax.inject.Inject;
+
 public class MainActivity extends AppCompatActivity {
+
+    @Inject
+    UserRepository userRepository;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        ((DaggerApplication) getApplicationContext()).applicationComponent.inject(this);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        boolean isCreated = userRepository.isCreated("01");
     }
 }
